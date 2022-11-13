739587be43a40639c688a0a2ad3e1452915268aa
j-up <ad4674@gmail.com>
2022-11-13 23:49:49 +0900

    [BasicsCodelab] state를 이용한 버튼 클릭 이벤트 추가

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,27 +39,34 @@ fun MyApp(
     }
 }
 
+
 @Composable
 private fun Greeting(name: String) {
+    // 내부 상태 추가를 위해 mutableStateOf 추가
+    // 리컴포지션 간에 상태를 유지하기위해 remember 사용 (리컴포지션 방지)
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
 }
-
 @Preview(showBackground = true, name = "default preview", widthDp = 320)
 @Composable
 fun DefaultPreview() {
