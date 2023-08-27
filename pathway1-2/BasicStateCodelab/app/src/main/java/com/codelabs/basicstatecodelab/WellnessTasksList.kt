a6a85243629e2ff9574c43fbdc4399eeaebf64b5
Lee JiMin <ad4674@gmail.com>
2023-08-28 02:14:58 +0900

    [BasicStateCodelab] 뷰모델 추가하여 비즈니스 로직 분리

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTasksList.kt
@@ -3,13 +3,13 @@ package com.codelabs.basicstatecodelab
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTasksList(
     modifier: Modifier = Modifier,
     onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     list: List<WellnessTask>,
 ) {
     LazyColumn(modifier = modifier) {
@@ -17,7 +17,12 @@ fun WellnessTasksList(
             items = list,
             key = { task -> task.id }
         ) { task ->
-            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) }
+            )
         }
     }
 }
