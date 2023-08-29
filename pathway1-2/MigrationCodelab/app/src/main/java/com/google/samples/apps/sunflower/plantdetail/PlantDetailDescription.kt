7ad657863b13cc8df9b16b8f61c4b8b4512243b4
Lee JiMin <ad4674@gmail.com>
2023-08-29 18:58:27 +0900

    [MigrationCodelab] 기존 View -> Compose View 대체

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -19,7 +19,9 @@ package com.google.samples.apps.sunflower.plantdetail
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun PlantDetailDescription() {
     Surface {
